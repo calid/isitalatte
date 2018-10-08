9850f0706d4bb1f9bd9e66fc2a6c19cdc7d98c59
Dylan Cali <calid1984@gmail.com>
2018-10-07 20:00:15 -0400

    some formatting tweaks

--- isitalatte.py
+++ isitalatte.py
@@ -16,7 +16,9 @@ parser.add_argument('image', type=str,
 
 args = parser.parse_args()
 
+print('Loading model...')
 model = tfk.models.load_model('isitalatte.h5')
+print('Done.')
 
 test_image = image.load_img(args.image, target_size=(150,150))
 test_image = image.img_to_array(test_image)
@@ -26,12 +28,13 @@ result = model.predict(test_image)
 
 # Class 0 = Latte
 # Class 1 = Not-Latte
-# (order the fit generator found the example directories during training)
+# This is the order the generator found the image directories during training
 
 # result is the probability the image belongs to class 1, not-latte
 not_latte_prob = result[0][0]
 latte_prob     = 1.0 - not_latte_prob
 
+print()
 if latte_prob > 0.7:
     print("It's a latte!")
 elif not_latte_prob > 0.7:
@@ -40,5 +43,5 @@ else:
     print("Not sure if latte?")
 
 print()
-print("Latte: %0.3f%%" % (latte_prob*100))
-print("Not Latte: %0.3f%%" % (not_latte_prob*100))
+print("Latte: %0.3f" % (latte_prob))
+print("Not Latte: %0.3f" % (not_latte_prob))
